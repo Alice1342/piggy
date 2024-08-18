fb05f75fbabc76931897ab1d58975a9708c92331
Alisa Orekhova <alisa.orekhova.main@gmail.com>
2024-08-18 14:01:37 +0500

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -15,4 +15,4 @@ def current_datetime(request):
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
